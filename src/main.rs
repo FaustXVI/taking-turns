e52040b0bea28595e0789d8fcc8d2d41cebe2fe9
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2025-09-17 23:55:00 +0200

    Poc for dates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::time::SystemTime;
+use chrono::{DateTime, NaiveDate, Utc};
+use dioxus::logger::tracing;
 use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
@@ -9,19 +12,26 @@ fn main() {
 
 #[component]
 fn App() -> Element {
+    let mut date_str: Signal<String> = use_signal(|| Utc::now().date_naive().to_string());
+    let date: Memo<Option<NaiveDate>> =
+        use_memo(move || NaiveDate::parse_from_str(&date_str(), "%Y-%m-%d").ok());
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
-
-    }
-}
-
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
         h1{
             "🎉 Taking turns ! 🎉"
+        }
+        input {
+            type: "date",
+            value : "{date_str}",
+            onchange: move |e| {
+                date_str.set(e.value());
+            }
+        }
+        if let Some(d) = date() {
+            div {
+                "Selected date : {d}"
+            }
         }
     }
 }
